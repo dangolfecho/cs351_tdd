53799393a883e80b749d71ec8f7999c4157878b0
Abineth Anantharam <98078409+abinetha@users.noreply.github.com>
2024-04-04 14:29:15 +0530

    Update tictactoe.py
    Updated init, print_board functions for satisfying initial tests

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,13 @@
 class TicTacToe:
     def __init__(self):
-        pass
+        self.board = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+        ]
+        self.current_player = 'X'
+
+    def print_board(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5)
